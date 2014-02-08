e83aa452a299e2a79b443134a7ba8e89642630f0
Ken Lu <ken@seeice.com>
2014-02-08 18:11:47 -0500

    some more javascript for sumbit the request form

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,18 @@
 class WelcomeController < ApplicationController
-  before_filter :authenticate_user!, only: [:request_list]
+  before_filter :authenticate_user!, only: [:request_list, :add_request]
   def index
   end
 
   def request_list
     @request_list = RequestRelation.all
+  end
+
+  def add_request
+    current_user.generate_request(request_params)
+  end
+
+  private
+  def request_params
+    params.require(:req).permit(:amount)
   end
 end
