752bccb2bfc3d1521cdc3d1acf39bc36f77971f8
Ken Lu <ken@seeice.com>
2014-02-08 16:30:08 -0500

    added user to login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Insight2::Application.routes.draw do
+  devise_for :users
   get 'welcome/index'
   root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
