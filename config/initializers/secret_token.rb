3a301c74cc8925454155dc7daa67c00e0b362190
Ken Lu <ken@seeice.com>
2014-02-08 15:37:52 -0500

    first init insight2 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Insight2::Application.config.secret_key_base = 'f1431ac0444ac8db44cb8a79d0060bf20a30b70623b08ef766c08b86d6e047eff3b5d89a5cd9ddbbccba320a8aa8b75b20bb57f7b5734d2289cd692c86ff4a68'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Insight2::Application.config.session_store :cookie_store, key: '_insight2_session'
