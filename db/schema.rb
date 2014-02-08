5d3bcda7661588d49478680bf344027605bf39c7
Ken Lu <ken@seeice.com>
2014-02-08 17:29:40 -0500

    added relation between relation and user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140208215756) do
+ActiveRecord::Schema.define(version: 20140208222730) do
+
+  create_table "request_relations", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "amount"
+    t.string   "dstatus"
+    t.datetime "request_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "requests", force: true do |t|
     t.integer  "user_id"
