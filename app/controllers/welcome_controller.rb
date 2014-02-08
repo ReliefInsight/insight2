983d7a77a913ff52bc14b9fa4a6ac8252fd6ed54
Ken Lu <ken@seeice.com>
2014-02-08 17:23:31 -0500

    rename to request list to avoid further problem

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,8 @@
 class WelcomeController < ApplicationController
+  before_filter :authenticate_user!, only: [:request_list]
   def index
   end
 
-  def request
+  def request_list
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Insight2::Application.routes.draw do
   devise_for :users
-  get 'welcome/index'
+  get '/wel', to: 'welcome#index'
+  get '/req_list', to: 'welcome#request_list'
   root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
